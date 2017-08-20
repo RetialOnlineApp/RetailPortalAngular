d87083dea185ac9d2014296c1f53ce57dcc508be
vivek munjewar <developer-hyd@gmail.com>
2017-08-20 18:02:09 +0530

    login & dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		DashboardComponent
+	],
+	imports: [
+		BrowserModule,
+		RouterModule.forRoot([
+			{
+				path: 'login',
+				component: LoginComponent
+			},
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			},
+			{
+				path: '',
+				redirectTo: '/login',
+				pathMatch: 'full'
+			},
+
+		])
+	],
+	providers: [],
+	bootstrap: [LoginComponent]
 })
 export class AppModule { }
