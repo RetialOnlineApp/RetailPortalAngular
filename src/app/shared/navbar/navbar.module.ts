2c2d46e1792fc6b5a4cf3164edd141942a5537af
vivek munjewar <developer-hyd@gmail.com>
2017-08-26 21:47:28 +0530

    settings module separated

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
+import {SettingsComponent} from './settings/settings.component';;
+
 
 @NgModule({
     imports: [ RouterModule, CommonModule ],
-    declarations: [ NavbarComponent ],
-    exports: [ NavbarComponent ]
+    declarations: [ NavbarComponent, SettingsComponent ],
+    exports: [ NavbarComponent, SettingsComponent ]
 })
 
 export class NavbarModule {}
---
+++ src/app/shared/navbar/settings/settings.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'settings-cmp',
+    templateUrl: 'settings.component.html'
+})
+
+export class SettingsComponent {
+   
+}
